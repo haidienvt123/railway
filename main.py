424036d92a8a2d3cd631b14e8be1f4524f3806dd
rtr_intern_haihp <haihp.work@gmail.com>
2023-12-08 18:26:53 +0700

    test port

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -22,4 +23,4 @@ def index():
     return "OK"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
